be6bc4c4609c10c8187f840bc81e869d094ba790
ubong <ubongabasi398@gmail.com>
2021-05-13 12:06:40 +0100

    third commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
 var mongoose = require("mongoose");
-// var expressSanitizer = require("express-sanitizer")
+var expressSanitizer = require("express-sanitizer")
 var express = require("express");
 var app = express();
 
